eae8f3429d1827f8e89227aa946ac205af1ce285
Krisztina Magyari <makriszta@yahoo.com>
2019-02-18 14:14:33 +0100

    Content added to Book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,13 +2,27 @@ import React from 'react'
 
 class Book extends React.Component {
   render () {
+
+    let hasThumbnail
+        if (this.props.book.imageLinks) {
+            hasThumbnail = this.props.book.imageLinks.thumbnail
+        } else {
+            hasThumbnail = ''
+        }
+
+    let whenChanging = (event) =>	{
+			this.props.changeShelf(this.props.book, event.target.value)
+		}
+
+    whenChanging = whenChanging.bind(this)
+
     return (
       <li>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url${this.props.book.imageLinks.thumbnail})`}}></div>
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url${hasThumbnail}`}}></div>
             <div className="book-shelf-changer">
-              <select>
+              <select onChange={whenChanging} value={this.props.currentShelf}>
                 <option value="move" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
